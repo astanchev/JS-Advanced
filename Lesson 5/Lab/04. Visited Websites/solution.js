381382e79b13f303b79833e6c1aa3e10381ec6a4
astanchev <mr_stanchev@hotmail.com>
2020-01-06 20:38:19 +0200

    Add comments for Lab problem 1

--- Lesson 5/Lab/04. Visited Websites/solution.js
+++ Lesson 5/Lab/04. Visited Websites/solution.js
@@ -8,6 +8,8 @@ function solve() {
   }
 
   function increaseCount() {
+    //'a href:' (the link) and 'p'(the text with counter) are both children of
+    //parent with className 'link-1', therefor they are siblings
     let pText = this.nextElementSibling;
     let [firstWord, count, lastWord] = pText.textContent.split(' ');
     pText.textContent = `${firstWord} ${++count} ${lastWord}`;
