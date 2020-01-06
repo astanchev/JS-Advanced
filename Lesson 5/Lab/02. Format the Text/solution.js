acd881b36a1e9f86eba3da5ce09882a7097ac73a
astanchev <mr_stanchev@hotmail.com>
2020-01-06 20:28:30 +0200

    Add Lesson 5 Lab

--- Lesson 5/Lab/02. Format the Text/solution.js
+++ Lesson 5/Lab/02. Format the Text/solution.js
@@ -1,22 +1,26 @@
 function solve() {
   let input = document.getElementById('input');
   let output = document.getElementById('output');
+
   let sentences = input
-    .innerText
+    .textContent
     .split('.')
     .filter(s => s !== '')
-    .map(s => s + '.');
+    .map(s => s + '\.');
 
-  while(sentences.length !== 0){
+  while (sentences.length !== 0) {
     let p = document.createElement('p');
+    let text = '';
 
     for (let i = 0; i < 3; i++) {
-        if (sentences.length === 0) {
-          break;
-        }         
-        p.innerText += sentences.shift();
+      if (sentences.length === 0) {
+        break;
+      }
+
+      text += sentences.shift();
     }
 
+    p.textContent += text;
     output.appendChild(p);
   }
 }
--- Lesson 5/Lab/04. Visited Websites/solution.js
+++ Lesson 5/Lab/04. Visited Websites/solution.js
@@ -1,4 +1,16 @@
 function solve() {
+  let links = document.getElementsByTagName('a');
 
-  //TODO...
+  for (const link of links) {
+    link
+      .addEventListener('click',
+        increaseCount);
+  }
+
+  function increaseCount() {
+    let pText = this.nextElementSibling;
+    let [firstWord, count, lastWord] = pText.textContent.split(' ');
+    pText.textContent = `${firstWord} ${++count} ${lastWord}`;
+  }
+
 }
