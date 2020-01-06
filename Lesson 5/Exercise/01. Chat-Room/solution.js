ce36413e38881ec54b6fe9670c336e98d019ffdf
astanchev <mr_stanchev@hotmail.com>
2020-01-06 21:30:21 +0200

    Add Lesson 5 exercise 1

--- Lesson 5/Exercise/01. Chat-Room/solution.js
+++ Lesson 5/Exercise/01. Chat-Room/solution.js
@@ -1,5 +1,19 @@
 function solve() {
-   //TODO...
+   const chatMessages = document.getElementById('chat_messages');
+   const div = document.createElement('div');
+   const bttn = document.getElementById('send');
+
+   bttn.addEventListener('click', addMessage);
+
+   function addMessage() {
+      const input = document.getElementById('chat_input');
+      const newDiv = div.cloneNode();
+
+      newDiv.setAttribute('class', 'message my-message');
+      newDiv.textContent = input.value;
+      chatMessages.appendChild(newDiv);
+      input.value = '';
+   }
 }
 
 
