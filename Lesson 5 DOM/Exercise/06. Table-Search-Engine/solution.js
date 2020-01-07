6228c7dcd2019fe20c44ec769baa986c7ce7fe1a
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-07 10:57:41 +0200

    Add Lesson 5 exercise 6

--- Lesson 5 DOM/Exercise/06. Table-Search-Engine/solution.js
+++ Lesson 5 DOM/Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+   const tdList = Array.from(document.querySelectorAll('tbody tr td'));
+   tdList.forEach((td, i) => td.setAttribute('id', i));
+
+   const btn = document.getElementById('searchBtn');
+   btn.addEventListener('click', searchAndMark);
+
+   function searchAndMark(params) {
+      const rows = Array.from(document.querySelectorAll('tbody tr'));
+      const query = document.getElementById('searchField').value.toLowerCase();
+
+      rows.forEach(r => r.setAttribute('class', ''));
+
+      if (query.length === 0) {
+         return;
+      }
+
+      tdList
+         .filter((x) => x.textContent
+            .toLowerCase()
+            .includes(query))
+         .forEach((x) => tdList[x.id]
+            .parentElement
+            .setAttribute('class', 'select'));
+
+      query.value = '';
+   }
 }
