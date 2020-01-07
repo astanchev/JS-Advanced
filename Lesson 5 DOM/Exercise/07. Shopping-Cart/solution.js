2f515d054e53f53bc6bd4263908a7c317609e30e
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-07 12:52:19 +0200

    Add Lesson 5 exercise 7

--- Lesson 5 DOM/Exercise/07. Shopping-Cart/solution.js
+++ Lesson 5 DOM/Exercise/07. Shopping-Cart/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-   //TODO...
+   const cart = new Map();
+   const textarea = document.getElementsByTagName('textarea')[0];
+   const btnCheckOut = document.getElementsByClassName('checkout')[0];
+   const addProducts = Array.from(document.getElementsByClassName('add-product'));
+   addProducts.forEach(b => b.addEventListener('click', addToCart));
+
+   btnCheckOut.addEventListener('click', checkOut);
+
+   function addToCart() {
+      let productName = this.parentElement
+         .previousElementSibling
+         .firstElementChild
+         .textContent;
+      let productPrice = this.parentElement
+         .nextElementSibling
+         .textContent;
+
+      textarea.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
+
+      if (!cart.has(productName)) {
+         cart.set(productName, 0);
+      }
+
+      let newSum = cart.get(productName) + Number(productPrice);
+      cart.set(productName, newSum);
+   }
+
+   function checkOut() {
+      const products = Array.from(cart.keys());
+      const displayedProducts = products.join(', ');
+      const sum = (Array.from(cart.values())).reduce((a, b) => a + b, 0);
+
+      textarea.textContent += `You bought ${displayedProducts} for ${sum.toFixed(2)}.`;
+
+      btnCheckOut.removeEventListener('click', checkOut);
+      addProducts.forEach(b => b.removeEventListener('click', addToCart));
+   }
 }
