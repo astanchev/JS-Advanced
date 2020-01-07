160a39322edf132f63c107ea603d27abdbe78433
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-07 18:49:19 +0200

    Add Lesson 5 exercise 9

--- Lesson 5 DOM/Exercise/09. Cards/solution.js
+++ Lesson 5 DOM/Exercise/09. Cards/solution.js
@@ -1,3 +1,41 @@
 function solve() {
-   // TODO...
+   const imgs = Array.from(document.getElementsByTagName('img'));
+   imgs.forEach((img) => img.addEventListener('click', showCard));
+
+   function showCard() {
+      let card = this;
+      card.src = './images/whiteCard.jpg';
+      card.removeEventListener('click', showCard);
+      
+      let spans = document.getElementById('result').children;
+      let [leftSpan, rightSpan] = [spans[0], spans[2]];
+
+      if (card.parentNode.id === 'player1Div') {
+         leftSpan.textContent = card.name;
+
+      } else if (card.parentNode.id === 'player2Div') {
+         rightSpan.textContent = card.name;
+      }
+
+      if (spans[0].textContent && spans[2].textContent) {
+         let winner;
+         let loser;
+
+         if (Number(leftSpan.textContent) > Number(rightSpan.textContent)) {
+            winner = document.querySelector(`#player1Div img[name='${leftSpan.textContent}']`);
+            loser = document.querySelector(`#player2Div img[name='${rightSpan.textContent}']`);
+
+         } else if (+leftSpan.textContent < +rightSpan.textContent) {
+            loser = document.querySelector(`#player1Div img[name='${leftSpan.textContent}']`);
+            winner = document.querySelector(`#player2Div img[name='${rightSpan.textContent}']`);
+         }
+
+         winner.style.border = '2px solid green';
+         loser.style.border = '2px solid red';
+
+         document.getElementById('history').textContent += `[${leftSpan.textContent} vs ${rightSpan.textContent}] `;
+
+         [leftSpan.textContent, rightSpan.textContent] = ['', ''];
+      }
+   }
 }
