13af429f3816e78b71d5f105ac3ccd750de42876
astanchev <mr_stanchev@hotmail.com>
2020-01-08 18:16:24 +0200

    Add Lesson 6 Lab 3

--- Lesson 6 DOM Manipulations/Lab/03. Delete-From-Table/app.js
+++ Lesson 6 DOM Manipulations/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const rows = Array.from(document.getElementsByTagName('tr')).slice(1);
+    const input = document.getElementsByName('email')[0];
+    const inputText = input.value;
+    const result = document.getElementById('result');
+
+    const indexToDelete = rows
+        .findIndex(r => r.lastElementChild.textContent === inputText);
+
+    if (indexToDelete > -1) {
+        const row = rows[indexToDelete];
+        const table = row.parentElement;
+
+        table.removeChild(row);
+        result.textContent = 'Deleted';
+    } else {
+        result.textContent = 'Not found.';
+    }
+    
+    input.value = '';
 }
