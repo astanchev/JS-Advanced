6b92e607d3c0700d1cdab54160ec89b0b27aba63
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-08 21:32:34 +0200

    Add Lesson 6 Lab 6, 7

--- Lesson 6 DOM Manipulations/Lab/06. Highlight-Active/app.js
+++ Lesson 6 DOM Manipulations/Lab/06. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focus() {
-    console.log('TODO:...');
+    const inputs = Array.from(document.getElementsByTagName('input'));
+    const sections = Array.from(document.querySelectorAll('body > div div'));
+    inputs.forEach(s => s.addEventListener('focus', setFocus));
+
+    function setFocus() {
+        this.parentElement.setAttribute('class', 'focused');
+        //console.log('setFocus focus ' + this.parentElement.textContent)
+        this.addEventListener('blur', removeClass);
+        //console.log('setFocus blur ' + this.parentElement.textContent)
+    }
+
+    function removeClass() {
+        //console.log('removeClass in ' + this.parentElement.textContent)
+        this.parentElement.removeAttribute('class');
+        //console.log('removeClass out ' + this.parentElement.textContent)
+    }
 }
--- Lesson 6 DOM Manipulations/Lab/07. Dynamic-Validation/app.js
+++ Lesson 6 DOM Manipulations/Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document.getElementById('email');
+    input.addEventListener('change', checkEmail);
+
+    function checkEmail(e) {
+        if (
+            e.target.value.match(/^[a-z]+@[a-z]+\.[a-z]+$/) === null) {
+            e.target.classList.add('error');
+        } else {
+            e.target.classList.remove('error');
+        }
+    }
 }
