24e48e75333040256da866eda01b999c56b05e29
astanchev <mr_stanchev@hotmail.com>
2020-01-08 19:08:00 +0200

    add

--- Lesson 6 DOM Manipulations/Lab/05. Mouse-Gradient/app.js
+++ Lesson 6 DOM Manipulations/Lab/05. Mouse-Gradient/app.js
@@ -1,3 +1,22 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const result = document.getElementById('result');
+
+    gradient.addEventListener('click', displayGradient);
+
+    function calculatePercentage(x, y) {
+        return Math.floor(x / y * 100);
+    }
+
+    function resultDivTemplateString(o) {
+        return `${o}%`;
+    }
+
+    function displayGradient(e) {
+        const clickPoint = e.offsetX;
+        const elWidth = gradient.offsetWidth;
+        const percent = calculatePercentage(clickPoint, elWidth);
+
+        result.textContent = resultDivTemplateString(percent);
+    }
 }
