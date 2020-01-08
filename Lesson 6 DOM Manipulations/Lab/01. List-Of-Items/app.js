377a7a40ac5732d6b03d35091c9bdd6e16c35536
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-08 17:03:23 +0200

    Add Lesson 6 Lab 1,2

--- Lesson 6 DOM Manipulations/Lab/01. List-Of-Items/app.js
+++ Lesson 6 DOM Manipulations/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    const items = document.getElementById('items');
+    const liToAdd = document.createElement('li');
+    const inputField = document.getElementById('newItemText');
+
+    liToAdd.textContent = inputField.value;
+    items.appendChild(liToAdd);
+    inputField.value = '';
 }
--- Lesson 6 DOM Manipulations/Lab/02. Add-Delete/app.js
+++ Lesson 6 DOM Manipulations/Lab/02. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    const items = document.getElementById('items');
+    items.addEventListener('click', deleteElement);
+
+    const inputField = document.getElementById('newText');
+
+    const liToAdd = document.createElement('li');
+    const liText = document.createTextNode(`${inputField.value} `);
+
+    const a = document.createElement('a');
+    const aText = document.createTextNode('[Delete]');    
+    a.appendChild(aText);
+    a.setAttribute("href", "#");
+    liToAdd.appendChild(liText);
+
+    liToAdd.appendChild(a);
+
+    items.appendChild(liToAdd);
+    inputField.value = '';
+
+    function deleteElement(e) {
+        this.removeChild(e.target.parentElement);
+    }
 }
