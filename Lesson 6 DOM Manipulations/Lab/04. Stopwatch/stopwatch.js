437b46808919aa48451848a02cedfaad7396b97a
astanchev <mr_stanchev@hotmail.com>
2020-01-08 18:40:06 +0200

    Add Lesson 6 lab 4

--- Lesson 6 DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
+++ Lesson 6 DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,27 @@
 function stopwatch() {
-    console.log('TODO:...');
+    const startBtn = document.getElementById('startBtn');
+    const stopBtn = document.getElementById('stopBtn');
+    const timer = document.getElementById('time');
+    let currentTime = 0;
+
+    startBtn.addEventListener('click', startTimer);
+
+    stopBtn.addEventListener('click', stopTimer);
+
+    function stopTimer() {
+        clearInterval(currentTime);
+        [startBtn.disabled, stopBtn.disabled] = [!startBtn.disabled, !stopBtn.disabled];
+    }
+
+    function startTimer() {
+        timer.textContent = '00:00';
+        let elapsedTime = 0;
+        currentTime = setInterval(function () {
+            elapsedTime++;
+            const minutes = Math.trunc(elapsedTime / 60).toString().padStart(2, '0');
+            const seconds = Math.trunc(elapsedTime % 60).toString().padStart(2, '0');
+            timer.textContent = minutes + ':' + seconds;
+        }, 1000);
+        [startBtn.disabled, stopBtn.disabled] = [!startBtn.disabled, !stopBtn.disabled];
+    }
 }
