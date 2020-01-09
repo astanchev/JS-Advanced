7f5346c0c1c6d9530d166ca37a8f75a132c8b9ac
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-09 21:09:06 +0200

    Add Lesson 7 Lab

---
+++ Lesson 7 Classes/Lab/03. Get Persons.js
@@ -0,0 +1,21 @@
+function solve() {
+    class Person{
+        constructor(firstName, lastName, age, email){
+            this.firstName = firstName;
+            this.lastName = lastName; 
+            this.age = age; 
+            this.email = email;
+        }
+    
+        toString(){
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+
+    let person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let person1 = new Person('SoftUni');
+    let person2 = new Person('Stephan', 'Johnson', 25);
+    let person3 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    return [person, person1, person2, person3];
+}
---
+++ Lesson 7 Classes/Lab/06. Cards.js
@@ -0,0 +1,52 @@
+let result = (function () {
+    const validateFace = function (face) {
+        return ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"].indexOf(face);
+    };
+
+    const Suits = { SPADES: '♠', HEARTS: '♥', DIAMONDS: '♦', CLUBS: '♣' };
+
+    const validateSuit = function (suit) {
+        return Object.values(Suits).includes(suit);
+    };
+
+    class Card {
+        constructor(face, suit) {
+            this.face = face;
+            this.suit = suit;
+        }
+
+        get face() {
+            return this._face;
+        }
+
+        set face(face) {
+            if (validateFace(face) === -1) {
+                throw new Error('invalid face');
+            }
+
+            this._face = face;
+        }
+
+        get suit() {
+            return this._suit;
+        }
+
+        set suit(suit) {
+            if (validateSuit(suit) === false) {
+                throw new Error('invalid suit');
+            }
+
+            this._suit = suit;
+        }
+    }
+
+    return { Suits, Card };
+})();
+
+let Card = result.Card;
+let Suits = result.Suits;
+
+let card = new Card('Q', Suits.CLUBS);
+card.face = 'A';
+card.suit = Suits.DIAMONDS;
+let card2 = new Card('1', Suits.DIAMONDS);
