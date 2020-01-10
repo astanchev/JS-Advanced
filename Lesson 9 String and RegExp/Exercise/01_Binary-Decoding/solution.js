8698f4090af1c0f64eb182cec3c9df21fe9a475d
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-01-11 01:01:16 +0200

    Add Lesson 9 Exercise 1

--- Lesson 9 String and RegExp/Exercise/01_Binary-Decoding/solution.js
+++ Lesson 9 String and RegExp/Exercise/01_Binary-Decoding/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-	//TODO...
+	const input = document.getElementById('input').value;
+	const output = document.getElementById('resultOutput');
+	const removeNum = sum(input);
+	const length = input.length - 2 * removeNum;
+
+	output.textContent = input
+		.substr(removeNum, length)
+        .split(/(\d{8})/)
+		.map(a => parseInt(a, 2))
+		.map(a => String.fromCharCode(a))
+		.filter((x) => /[A-Za-z ]+/g.test(x))
+		.join('');
+
+	function sum(input) {
+
+		if (input.length === 1) {
+			return +input;
+		}
+
+		return sum(input
+			.split('')
+			.map(Number)
+			.reduce((a, b) => a + b, 0)
+			.toString());
+	}
 }
