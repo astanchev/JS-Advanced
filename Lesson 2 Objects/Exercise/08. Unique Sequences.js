4df4e5536a5f6f6bf7d83afa2003ed0cbfa4071b
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-05-31 21:22:02 +0300

    new solution

--- Lesson 2 Objects/Exercise/08. Unique Sequences.js
+++ Lesson 2 Objects/Exercise/08. Unique Sequences.js
@@ -13,6 +13,23 @@ function solve(input) {
         .forEach((x) => console.log(`[${x.join(', ')}]`));
 }
 
+function solve2(input) {
+    let set = new Set();
+
+    for (const line of input) {
+        const orderedLine = JSON.parse(line).sort((a, b) => b - a);
+
+        set.add(JSON.stringify(orderedLine));
+    }
+
+    let result = Array.from(set);
+
+    result
+        .map((x) => JSON.parse(x))
+        .sort((a, b) => a.length - b.length)
+        .forEach((x) => console.log(`[${x.join(', ')}]`));
+}
+
 solve([
     "[-3, -2, -1, 0, 1, 2, 3, 4]",
     "[10, 1, -17, 0, 2, 13]",
--- Test.js
+++ Test.js
@@ -1,5 +1,6 @@
 function solve(input) {
-
+    
+    
 }
 
 solve();
