8964dbcac30f76d468afcb3cb9378af58df54145
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-03 15:01:07 +0300

    refactor

--- Lesson 5 DOM/Exercise/07. Shopping-Cart/solution.js
+++ Lesson 5 DOM/Exercise/07. Shopping-Cart/solution.js
@@ -1,39 +1,33 @@
 function solve() {
-   const cart = new Map();
-   const textarea = document.getElementsByTagName('textarea')[0];
-   const btnCheckOut = document.getElementsByClassName('checkout')[0];
-   const addProducts = Array.from(document.getElementsByClassName('add-product'));
-   addProducts.forEach(b => b.addEventListener('click', addToCart));
+   const products = new Set();
+   let totalAmount = 0;
 
-   btnCheckOut.addEventListener('click', checkOut);
+   const output = document.getElementsByTagName('textarea')[0];
+   const addBtns = [...document.querySelectorAll('div.product-add')];
+   addBtns.forEach(b => b.addEventListener('click', addToCart));
+   const checkOutBtn = document.querySelector('.checkout');
+   checkOutBtn.addEventListener('click', checkOut);
 
    function addToCart() {
-      let productName = this.parentElement
-         .previousElementSibling
-         .firstElementChild
-         .textContent;
-      let productPrice = this.parentElement
-         .nextElementSibling
-         .textContent;
+      const productName = this.previousElementSibling.firstElementChild.textContent;
+      const productPrice = Number(this.nextElementSibling.textContent);
 
-      textarea.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
-
-      if (!cart.has(productName)) {
-         cart.set(productName, 0);
-      }
-
-      let newSum = cart.get(productName) + Number(productPrice);
-      cart.set(productName, newSum);
+      products.add(productName);
+      totalAmount += productPrice;
+      output.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
    }
 
    function checkOut() {
-      const products = Array.from(cart.keys());
-      const displayedProducts = products.join(', ');
-      const sum = (Array.from(cart.values())).reduce((a, b) => a + b, 0);
 
-      textarea.textContent += `You bought ${displayedProducts} for ${sum.toFixed(2)}.`;
+      const list = [...products].join(', ');
 
-      btnCheckOut.removeEventListener('click', checkOut);
-      addProducts.forEach(b => b.removeEventListener('click', addToCart));
+      output.value += `You bought ${list} for ${totalAmount.toFixed(2)}.`;
+
+      addBtns.forEach(b => b.firstElementChild.setAttribute('disabled', ''));
+      checkOutBtn.setAttribute('disabled', '');
+
+      //Or other ption is:
+      // addBtns.forEach(b => b.removeEventListener('click', addToCart));
+      // checkOutBtn.removeEventListener('click', checkOut);
    }
 }
