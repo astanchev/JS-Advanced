02a7a05c75e9cbf2c6f53913a802b7cb5e233463
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-05 11:26:55 +0300

    refactor

--- Lesson 6 DOM Manipulations/Lab/05. Mouse-Gradient/app.js
+++ Lesson 6 DOM Manipulations/Lab/05. Mouse-Gradient/app.js
@@ -1,39 +1,24 @@
 function attachGradientEvents() {
-    const gradient = document.getElementById('gradient');
-    const result = document.getElementById('result');
+  const gradient = document.getElementById('gradient');
+  const result = document.getElementById('result');
 
-    gradient.addEventListener('click', displayGradient);
+  gradient.addEventListener('mousemove', displayGradient);
 
-    function calculatePercentage(x, y) {
-        return Math.floor(x / y * 100);
-    }
-
-    function resultDivTemplateString(o) {
-        return `${o}%`;
-    }
-
-    function displayGradient(e) {
-        const clickPoint = e.offsetX;
-        const elWidth = gradient.offsetWidth;
-        const percent = calculatePercentage(clickPoint, elWidth);
-
-        result.textContent = resultDivTemplateString(percent);
-    }
-}
-
-function solve() {
-    function getPercentage(x) {
-      return Math.floor((x / gradientWidth) * 100);
-    }
-  
-    function getResult(evt) {
-      output.textContent = `${getPercentage(evt.offsetX)}%`;
-    }
-  
-    let output = document.getElementById("result");
-    let gradient = document.getElementById("gradient-box");
-    //let gradientWidth = Number(window.getComputedStyle(gradient).width.replace("px", ""));
-    let gradientWidth = 300;
-  
-    gradient.addEventListener("mousemove", getResult);
+  function calculatePercentage(x, y) {
+      return Math.floor(x / y * 100);
   }
+
+  function resultDivTemplateString(o) {
+      return `${o}%`;
+  }
+
+  function displayGradient(e) {
+      const clickPoint = e.offsetX;
+
+      //In Judge the width is calculated with 300, if not -> use for width [gradient.offsetWidth]
+      const elWidth = 300;
+      const percent = calculatePercentage(clickPoint, elWidth);
+
+      result.textContent = resultDivTemplateString(percent);
+  }
+}
