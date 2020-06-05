5408a30399a1f90a48412eb7684c70eeb7e2fbf8
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-05 12:02:41 +0300

    refactor

--- Lesson 6 DOM Manipulations/Lab/07. Dynamic-Validation/app.js
+++ Lesson 6 DOM Manipulations/Lab/07. Dynamic-Validation/app.js
@@ -1,13 +1,16 @@
 function validate() {
-    const input = document.getElementById('email');
-    input.addEventListener('change', checkEmail);
+    const inputField = document.getElementById('email');
+    const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/gim;
 
-    function checkEmail(e) {
-        if (
-            e.target.value.match(/^[a-z]+@[a-z]+\.[a-z]+$/) === null) {
-            e.target.classList.add('error');
-        } else {
-            e.target.classList.remove('error');
-        }
+    inputField.addEventListener('change', validateText);
+
+    function validateText(e) {
+        e.preventDefault();
+
+            if (!inputField.value.match(pattern)) {
+                inputField.classList.add('error');
+            } else {
+                inputField.classList.remove('error');
+            }
     }
 }
