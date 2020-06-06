7616c0df3e8ca9e22f5bf50d8f3eb90518fd0ead
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-06 19:41:42 +0300

    new solution

--- Lesson 6 DOM Manipulations/Exercise/06. Time-Converter/solution.js
+++ Lesson 6 DOM Manipulations/Exercise/06. Time-Converter/solution.js
@@ -36,3 +36,58 @@ function attachEventsListeners() {
             minutes.value = seconds.value / 60;
         });
 }
+
+function attachEventsListeners2() {
+    const convertBtns = [...document.querySelectorAll('input[value="Convert"]')];
+    const days = document.getElementById('days');
+    const hours = document.getElementById('hours');
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
+    const conversions = {
+        days: (x) => convertDays(x),
+        hours: (x) => convertHours(x),
+        minutes: (x) => convertMinutes(x),
+        seconds: (x) => convertSeconds(x)
+    };
+
+    convertBtns.forEach(b => b.addEventListener('click', convert));
+
+    function convert(e) {
+        e.preventDefault();
+
+        const valueToConvert = Number(e.target.previousElementSibling.value);
+
+        if (isNaN(valueToConvert)) {
+            e.target.previousElementSibling.value = '';
+            return;
+        }
+
+        const typeOfInput = e.target.parentElement.firstElementChild.textContent.split(':')[0].toLowerCase();
+
+        conversions[typeOfInput](valueToConvert);
+    }
+
+    function convertDays(x) {
+        hours.value = x * 24;
+        minutes.value = x * 60 * 24;
+        seconds.value = x * 60 * 60 * 24;
+    }
+
+    function convertHours(x) {
+        days.value = x / 24;
+        minutes.value = x * 60;
+        seconds.value = x * 60 * 60;
+    }
+
+    function convertMinutes(x) {
+        days.value = x / 60 / 24;
+        hours.value = x / 60;
+        seconds.value = x * 60;
+    }
+
+    function convertSeconds(x) {
+        days.value = x / 60 / 60 / 24;
+        hours.value = x / 60 / 60;
+        minutes.value = x / 60;
+    }
+}
