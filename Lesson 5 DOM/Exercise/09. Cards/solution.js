1d4f8bb174c2bc368ae59ee08a196eab9817ed50
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-07 14:47:43 +0300

    new solution

--- Lesson 5 DOM/Exercise/09. Cards/solution.js
+++ Lesson 5 DOM/Exercise/09. Cards/solution.js
@@ -39,3 +39,70 @@ function solve() {
       }
    }
 }
+
+function solve2() {
+   const player1Cards = Array.from(document.getElementById('player1Div').children);
+   const player2Cards = Array.from(document.getElementById('player2Div').children);
+   const history = document.getElementById('history');
+   const resultSpans = Array.from(document.getElementById('result').children);
+   const [player1Result, player2Result] = [resultSpans[0], resultSpans[2]];
+   let [player1, player2] = [null, null];
+   
+   player1Cards.forEach(c => c.addEventListener('click', showPlayer1Card));
+   player2Cards.forEach(c => c.addEventListener('click', showPlayer2Card));
+
+   function showPlayer1Card(e) {
+      e.preventDefault();
+      e.target.removeEventListener('click', showPlayer1Card);
+
+      e.target.src = 'images/whiteCard.jpg';
+      player1Result.textContent = e.target.name;
+      player1 = e.target;
+
+      if (check2Players) {
+         finishHand();
+      }     
+   }
+
+   function showPlayer2Card(e) {
+      e.preventDefault();
+      e.target.removeEventListener('click', showPlayer2Card);
+
+      e.target.src = 'images/whiteCard.jpg';
+      player2Result.textContent = e.target.name;
+      player2 = e.target;
+
+      if (check2Players) {
+         finishHand();
+      }
+   }
+
+   function addHistory(){
+      history.textContent += `[${player1Result.textContent} vs ${player2Result.textContent}] `;
+      player1Result.textContent = '';
+      player2Result.textContent = '';
+   }
+
+   function colorWinnerLoser(winner, loser){
+      winner.style.border = '2px solid green';
+      loser.style.border = '2px solid red';
+   }
+
+   function check2Players() {
+      return player1 && player2;
+   }
+
+   function finishHand() {
+      const player1Value = Number(player1.name);
+      const player2Value = Number(player2.name);
+
+      if (player1Value > player2Value) {
+         colorWinnerLoser(player1, player2);
+      } else if (player2Value > player1Value) {
+         colorWinnerLoser(player2, player1);
+      }
+
+      addHistory();
+      [player1, player2] = [null, null];
+   }
+}
