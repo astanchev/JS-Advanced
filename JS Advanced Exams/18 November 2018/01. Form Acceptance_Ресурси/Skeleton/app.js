ea6da5c558cf424f0877774ae289b3838adbfa6e
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-24 21:51:26 +0300

    Add solution for problem 1 exam 18/11/2020

--- JS Advanced Exams/18 November 2018/01. Form Acceptance_Ресурси/Skeleton/app.js
+++ JS Advanced Exams/18 November 2018/01. Form Acceptance_Ресурси/Skeleton/app.js
@@ -1,3 +1,64 @@
 function acceptance() {
-	// TODO...
+	const inputCompany = document.querySelector('input[name="shippingCompany"]');
+	const inputName = document.querySelector('input[name="productName"]');
+	const inputQuantity = document.querySelector('input[name="productQuantity"]');
+	const inputScrape = document.querySelector('input[name="productScrape"]');
+	const warehouse = document.getElementById('warehouse');
+
+	const company = inputCompany.value;
+	const name = inputName.value;
+	const quantity = Number(inputQuantity.value);
+	const scrape = Number(inputScrape.value);
+
+
+	if (company === '' || name === '' || isNaN(quantity) || isNaN(scrape)) {
+		return;
+	}
+
+	if (quantity <= scrape) {
+		return;
+	}
+
+	const btnOut = createElement('button', 'Out of stock', {type: 'button'});
+
+	const product = createElement('div', [
+		createElement('p', `[${company}] ${name} - ${quantity - scrape} pieces`),
+		btnOut
+	]);
+
+	btnOut.addEventListener('click', (e) => {
+		e.preventDefault();
+		e.target.parentElement.remove();
+	});
+
+	inputCompany.value = '';
+	inputName.value = '';
+	inputQuantity.value = '';
+	inputScrape.value = '';
+	warehouse.appendChild(product);
+
+
+	function createElement(type, content, attributes) {
+		const result = document.createElement(type);
+	
+		if (attributes !== undefined) {
+			Object.assign(result, attributes);
+		}
+	
+		if (Array.isArray(content)) {
+			content.forEach(append);
+		} else {
+			append(content);
+		}
+	
+		function append(node) {
+			if (typeof node === 'string') {
+				node = document.createTextNode(node);
+			}
+	
+			result.appendChild(node);
+		}
+	
+		return result;
+	}
 }
