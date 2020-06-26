9cac1773c01032843b7b3fc92a308fa2d911f082
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-26 18:04:36 +0300

    refactor

--- Test.js
+++ Test.js
@@ -23,7 +23,21 @@ const arr5 = [...arr1, ...arr2];
 // console.log();
 // console.log(arr5);
 
-console.log(Number('five') <= 0);
-console.log(Number('-5') <= 0);
-console.log(Number('5') <= 0);
+// console.log(Number('five') <= 0);
+// console.log(Number('-5') <= 0);
+// console.log(Number('5') <= 0);
 
+// console.log(!![]);
+// console.log(Boolean([]));
+// console.log(Boolean(0));
+// console.log(!!0);
+
+// console.log([] == true);
+// console.log([] === true);
+// console.log({} == true);
+// console.log({} === true);
+
+let arr7 = [ "John", "Peter", "Sally", "Jane" ];
+let myJSON = JSON.stringify(arr7);
+console.log(myJSON); // ["John","Peter","Sally","Jane"]
+console.log(arr7); // [ 'John', 'Peter', 'Sally', 'Jane' ]
